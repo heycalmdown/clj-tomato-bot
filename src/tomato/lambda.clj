2427334a39233cc1c1813ccd6917799c05c80c2b
heycalmdown <heyacct@gmail.com>
2016-11-24 07:16:31 +0900

    Change to use lambda as a backend

---
+++ src/tomato/lambda.clj
@@ -0,0 +1,28 @@
+(ns tomato.lambda
+  (:require [clojure.string :as s]
+            [cheshire.core :as json]
+            [clojure.java.io :as io]
+            [tomato.core :as tomato]
+            [tomato.handler :as handler])
+  (:gen-class
+    :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler]))
+
+(defn key->keyword [key-string]
+  (-> key-string
+      (s/replace #"([a-z])([A-Z])" "$1-$2")
+      (s/replace #"([A-Z]+)([A-Z])" "$1-$2")
+      (s/lower-case)
+      (keyword)))
+
+(defn -handleRequest [_this is _os _context]
+  (let [req (json/parse-string (slurp (io/reader is)) key->keyword)
+        text (:text (:message req))]
+    (if (s/starts-with? text "/")
+      (condp = (subs text 1)
+        "go" (handler/handle-start-session!)
+        "check" (handler/handle-check-remaining! (tomato/get-state!))
+        "count" (handler/handle-send-counted! (tomato/get-counted!))
+        "cancel" (handler/handle-cancel-session!)
+        "pause" (handler/handle-pause-session!)
+        "resume" (handler/handle-resume-session (tomato/get-state!))
+        (println text)))))
