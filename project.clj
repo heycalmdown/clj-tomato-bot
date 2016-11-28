3f95e362790d7252ce7fd4364a40ebc31f24573a
heycalmdown <heyacct@gmail.com>
2016-11-29 07:41:42 +0900

    Diet dependencies

--- project.clj
+++ project.clj
@@ -7,8 +7,12 @@
                  [morse "0.2.2" :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [clj-http "3.3.0"]
                  [clj-time "0.12.2"]
-                 [com.amazonaws/aws-lambda-java-core "1.1.0"]
-                 [amazonica "0.3.77" :exclusions []]]
+                 [amazonica "0.3.77" :exclusions [com.amazonaws/aws-java-sdk
+                                                  com.amazonaws/amazon-kinesis-client]]
+                 [com.amazonaws/aws-java-sdk-core "1.11.26"]
+                 [com.amazonaws/aws-java-sdk-s3 "1.11.26"]
+                 [com.amazonaws/aws-java-sdk-cloudwatch "1.11.26"]
+                 [com.amazonaws/aws-lambda-java-core "1.1.0"]]
   :plugins [[lein-cloverage "1.0.8"]]
   :main ^:skip-aot tomato.core
   :target-path "target/%s"
