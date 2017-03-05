ce450dc966c9e616c5ff6f1802cfbd71d9a6179d
heycalmdown <heyacct@gmail.com>
2017-03-05 09:52:54 +0900

    Bump dependencies up

--- project.clj
+++ project.clj
@@ -4,18 +4,18 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [morse "0.2.2" :exclusions [com.fasterxml.jackson.core/jackson-core]]
-                 [clj-http "3.3.0"]
-                 [clj-time "0.12.2"]
-                 [amazonica "0.3.77" :exclusions [
+                 [morse "0.2.4" :exclusions [com.fasterxml.jackson.core/jackson-core]]
+                 [clj-http "3.4.1"]
+                 [clj-time "0.13.0"]
+                 [amazonica "0.3.88" :exclusions [
                                                   com.amazonaws/aws-java-sdk
                                                   com.amazonaws/amazon-kinesis-client]]
-                 [com.amazonaws/aws-java-sdk-core "1.11.26"]
-                 [com.amazonaws/aws-java-sdk-s3 "1.11.26"]
-                 [com.amazonaws/aws-java-sdk-cloudwatch "1.11.26"]
-                 [com.amazonaws/aws-java-sdk-events "1.11.26"]
+                 [com.amazonaws/aws-java-sdk-core "1.11.98"]
+                 [com.amazonaws/aws-java-sdk-s3 "1.11.98"]
+                 [com.amazonaws/aws-java-sdk-cloudwatch "1.11.98"]
+                 [com.amazonaws/aws-java-sdk-events "1.11.98"]
                  [com.amazonaws/aws-lambda-java-core "1.1.0"]]
-  :plugins [[lein-cloverage "1.0.8"]]
+  :plugins [[lein-cloverage "1.0.9"]]
   :main ^:skip-aot tomato.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
